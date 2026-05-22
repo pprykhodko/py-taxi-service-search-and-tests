0d678bf4d955632b33446ccea7e0eda4da527fd7
Petro Prykhodko <petro.prykhodko2004@gmail.com>
2026-05-22 17:14:45 +0200

    solution 3

--- tests/test_driver_form.py
+++ tests/test_driver_form.py
@@ -15,10 +15,22 @@ class DriverFormTests(TestCase):
         }
         form = DriverCreationForm(data=form_data)
         self.assertTrue(form.is_valid())
-        self.assertEqual(form.cleaned_data["username"], form_data["username"])
-        self.assertEqual(form.cleaned_data["license_number"], form_data["license_number"])
-        self.assertEqual(form.cleaned_data["first_name"], form_data["first_name"])
-        self.assertEqual(form.cleaned_data["last_name"], form_data["last_name"])
+        self.assertEqual(
+            form.cleaned_data["username"],
+            form_data["username"]
+        )
+        self.assertEqual(
+            form.cleaned_data["license_number"],
+            form_data["license_number"]
+        )
+        self.assertEqual(
+            form.cleaned_data["first_name"],
+            form_data["first_name"]
+        )
+        self.assertEqual(
+            form.cleaned_data["last_name"],
+            form_data["last_name"]
+        )
 
     def test_invalid_license_number(self):
         form_data = {
